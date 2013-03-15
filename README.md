849c38bd871127fc3dfa6c19bbfb19a3be6c7ea6
Marco <marco@Marc-Oliviers-MacBook-Pro.local>
2013-03-15 22:18:27 +1300

    first add

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 remote-sensor-wifi
 ==================
 
-Monitor your home remotely using the Arduino WiFi Shield
+Code for the article: "Monitor your home remotely using the Arduino WiFi Shield". The description of the project is available at: http://openhomeautomation.net/monitor-your-home-remotely-using-the-arduino-wifi-shield/
---
+++ remote_sensor_full/remote_sensor_full.ino
@@ -0,0 +1,107 @@
+#include <SPI.h>
+#include <WiFi.h>
+
+char ssid[] = "Georgemanor";     //  your network SSID (name) 
+char pass[] = "1284MJ181916F";    // your network password
+
+int status = WL_IDLE_STATUS;
+
+boolean door_status = false;
+
+WiFiServer server(80);
+
+void setup() {
+  // start serial port for debugging purposes
+  Serial.begin(9600);
+  
+  // Attach interrupt to pin 2
+  attachInterrupt(0, setDoorStatus, RISING);
+
+  // attempt to connect to Wifi network:
+  while ( status != WL_CONNECTED) { 
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+    // wait 10 seconds for connection:
+    delay(10000);
+  } 
+  server.begin();
+  // you're connected now, so print out the status:
+  printWifiStatus();
+}
+
+
+void loop() {
+            
+  // listen for incoming clients
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connnection: close");
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          client.println("<meta http-equiv=\"refresh\" content=\"5\">");
+          
+            if (door_status == false){
+              client.print("Everything is ok");
+            }
+            else {
+               client.print("Alert ! The door has been opened");
+            }       
+          client.println("<br />");
+          client.println("</html>");
+           break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } 
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+      // close the connection:
+      client.stop();
+      Serial.println("client disonnected");
+  }
+}
+
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+void setDoorStatus() {
+  door_status = true;
+}
